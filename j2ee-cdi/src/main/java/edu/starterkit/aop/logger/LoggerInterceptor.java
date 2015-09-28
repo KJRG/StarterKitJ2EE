7054ae7f50a7f9be6bff89d8b6bf7a280f9eb826
KGALKA <krzysztof.galka@capgemini.com>
2015-09-28 15:18:23 +0200

    Using logger to print informations

--- j2ee-cdi/src/main/java/edu/starterkit/aop/logger/LoggerInterceptor.java
+++ j2ee-cdi/src/main/java/edu/starterkit/aop/logger/LoggerInterceptor.java
@@ -2,39 +2,65 @@ package edu.starterkit.aop.logger;
 
 import javax.interceptor.Interceptor;
 import javax.interceptor.InvocationContext;
+
+import java.lang.reflect.Parameter;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import javax.interceptor.AroundInvoke;
 
 @Logged
 @Interceptor
 public class LoggerInterceptor {
 	
+	private Logger logger = Logger.getGlobal();
+	
 	@AroundInvoke
 	public Object aroundInvokeMethod(InvocationContext context) throws Exception {
 		
-		System.out.println("Intercepting method call");
+		StringBuilder builder = new StringBuilder();
+		builder.append("Intercepting method call");
 		
 		/*
-		 * Print the called method.
+		 * Print the called method's name and parameters.
 		 */
-		System.out.println("Called method:\n" + context.getMethod().getName());
+		builder.append("\nCalled method: " + context.getMethod().getName() + "(");
+		List<Parameter> params = Arrays.asList(context.getMethod().getParameters());
+		ListIterator<Parameter> iter = params.listIterator();
+		while(iter.hasNext()) {
+			Parameter p = iter.next();
+			builder.append(p.getParameterizedType().toString() + " " + p.getName());
+			if(iter.hasNext()) {
+				builder.append(", ");
+			}
+		}
+		builder.append(")");
 		
 		/*
 		 * Print called method parameters.
 		 */
-		System.out.println("Parameters:");
-		for(Object param : context.getParameters()) {
-			System.out.println("Parameter: " + param.getClass().toString() + ", value: " + param.toString());
+		Object[] passedParams = context.getParameters();
+		if(passedParams != null && passedParams.length > 0) {
+			builder.append("\nParameters:");
+			for(Object param : passedParams) {
+				builder.append("\n\ttype: " + param.getClass().toString() + ", value: " + param.toString());
+			}
 		}
 		
 		/*
 		 * Call the method.
 		 */
+		builder.append("\nCalling method...");
+		logger.log(Level.INFO, builder.toString());
 		Object result = context.proceed();
 		
 		/*
 		 * Print the result.
 		 */
-		System.out.println("Result:\n" + result.toString());
+		logger.log(Level.INFO, "Result:\n" + result.toString());
 		
 		return result;
 	}
--- j2ee-cdi/src/main/java/edu/starterkit/observer/BooksObserver.java
+++ j2ee-cdi/src/main/java/edu/starterkit/observer/BooksObserver.java
@@ -1,12 +1,17 @@
 package edu.starterkit.observer;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import javax.enterprise.event.Observes;
 
 import edu.starterkit.to.BookTo;
 
 public class BooksObserver {
 
+	private Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+	
 	public void onBookToEvent(@Observes BookTo book) {
-		System.out.println("An event referring to book:\n" + book.toString() + "\nwas raised.");
+		logger.log(Level.INFO, "An event referring to book:\n" + book.toString() + "\nwas raised.");
 	}
 }
